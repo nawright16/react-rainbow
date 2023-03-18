a44be386f8d2ba5942b1e03ec8f2a0ef3eea14cc
Nick Wright <nwright@egnyte.com>
2023-03-18 14:14:53 -0400

    react rainbows

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import ColorBlock from './ColorBlock'
+import ColorForm from './ColorForm'
+import './App.css'
 
 function App() {
+  let [colors, setColors] = useState([])
+
+  let colorMap = colors.map((color, i) => {
+    return (
+      <ColorBlock color={color} key={i} />
+    )
+  })
+
+  const addColor = (newColor) => {
+    setColors([...colors, newColor])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {colorMap}
+     <ColorForm addColor={addColor} />
     </div>
-  );
-}
+
+  )}
+
 
 export default App;
---
+++ src/ColorForm.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+function ColorForm(props) {
+    let [input, setInput] = useState('')
+
+    const isColor = (strColor) => {
+        const s = new Option().style;
+        s.color = strColor;
+        return s.color !== '';
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (isColor(input)) {
+            props.addColor(input)
+        }
+        setInput('')
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text"
+                    onChange={(e) => setInput(e.target.value)}
+                    value={input} />
+
+                <button type="submit">Submit!</button>
+            </form>
+        </div>
+    )
+}
+
+export default ColorForm;
